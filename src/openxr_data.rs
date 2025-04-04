a175486eb6d1a395b5977c2577e4fca870fb87dc
Sarah <xxklintan123xx@gmail.com>
2025-04-04 15:46:42 +0200

    poll_events: move hmd fetching outside of loop

--- src/openxr_data.rs
+++ src/openxr_data.rs
@@ -184,10 +184,10 @@ impl<C: Compositor> OpenXrData<C> {
                     let xr_input = self.input.get().unwrap();
 
                     let devices = xr_input.devices.read().unwrap();
+                    let hmd = devices.get_hmd();
 
                     for hand in [TrackedDeviceType::LeftHand, TrackedDeviceType::RightHand] {
                         let controller = devices.get_controller(hand);
-                        let hmd = devices.get_hmd();
 
                         let profile_path = session_data
                             .session
